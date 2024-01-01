f57f1fca3d174aa8a11560f9de1e48b4f2c005fc
MinaChao <145426187+MinaChao@users.noreply.github.com>
2024-01-01 12:17:37 +0800

    更新

--- js/all.js
+++ js/all.js
@@ -5,14 +5,16 @@ const webDataUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1TUZOFns9dqeBf
 const illustrationDataUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1TUZOFns9dqeBfhOfxWJ3_9H4ACBtAycmm47yHpwF-Ek/values/illustration?alt=json&key=AIzaSyA1M5R1HsJnEvDl5C6HY4fYY8s7dsBG2zw';
 const clipDataUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1TUZOFns9dqeBfhOfxWJ3_9H4ACBtAycmm47yHpwF-Ek/values/clip?alt=json&key=AIzaSyA1M5R1HsJnEvDl5C6HY4fYY8s7dsBG2zw';
 const productDataUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1TUZOFns9dqeBfhOfxWJ3_9H4ACBtAycmm47yHpwF-Ek/values/product?alt=json&key=AIzaSyA1M5R1HsJnEvDl5C6HY4fYY8s7dsBG2zw';
+const otherDataUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1TUZOFns9dqeBfhOfxWJ3_9H4ACBtAycmm47yHpwF-Ek/values/other?alt=json&key=AIzaSyA1M5R1HsJnEvDl5C6HY4fYY8s7dsBG2zw';
 
 createApp({
     data() {
-        let portfolio_page=0;
+        let portfolio_page=2;
         let web_page = 3;
         return {
             uiuxData: [],
             webData: [],
+            illustrationData: [],
             portfolio_page,
             web_page
         };
@@ -66,6 +68,24 @@ createApp({
                 resultObjects.shift();
                 console.log(resultObjects);
                 this.webData=resultObjects;
+            })
+            .catch((error) => {
+                console.error('Error fetching web design data:', error);
+            });
+        axios.get(illustrationDataUrl)
+            .then((response) => {
+                // console.log(response.data.values);
+                this.illustrationData = response.data.values;
+
+                const resultObjects = this.illustrationData.map((item) => {
+                    return {
+                        img: item[0],
+                        tag: item[1],
+                    };
+                });
+                resultObjects.shift();
+                console.log(resultObjects);
+                this.illustrationData=resultObjects;
             })
             .catch((error) => {
                 console.error('Error fetching web design data:', error);
